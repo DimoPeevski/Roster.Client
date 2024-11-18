b6c1d98ec1edcaaaf200eace81fc3a8fe688b9b1
Dimo Peevski <didiprophet@gmail.com>
2024-11-18 15:49:45 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './common/header/header.component';
+import { FooterComponent } from './common/footer/footer.component';
+import { MainComponent } from './main/main.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    RouterOutlet, 
+    HeaderComponent, 
+    MainComponent, 
+    FooterComponent
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'Roster.Client';
+}
