ca57ab313312e906a73711ba378bc729f8298814
Dimo Peevski <didiprophet@gmail.com>
2024-12-09 23:45:55 +0200

    Logout functionality implemented.

--- src/app/user/logout/logout.component.ts
+++ src/app/user/logout/logout.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-logout',
   standalone: true,
   imports: [],
   templateUrl: './logout.component.html',
-  styleUrl: './logout.component.css'
+  styleUrl: './logout.component.css',
 })
 export class LogoutComponent {
+  constructor(private userServer: UserService, private router: Router) {}
 
+  logout() {
+    this.userServer.logout();
+    this.router.navigate(['/']);
+  }
+
+  cancelLogout() {
+    this.router.navigate(['/']);
+  }
 }
