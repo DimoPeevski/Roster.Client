4bc7e440ec475cc268e7ffe8d8c9392afb8dffb4
Dimo Peevski <didiprophet@gmail.com>
2024-12-11 15:27:18 +0200

    Added loaded.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './common/header/header.component';
 import { FooterComponent } from './common/footer/footer.component';
+import { AuthenticateComponent } from './authenticate/authenticate.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    HeaderComponent, 
+    HeaderComponent,
     RouterOutlet,
-    FooterComponent
+    FooterComponent,
+    AuthenticateComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
