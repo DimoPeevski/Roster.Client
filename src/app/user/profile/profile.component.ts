564f1f2d04e79bd554a8bfc8d2f127b5c18ac3ba
Dimo Peevski <didiprophet@gmail.com>
2024-12-14 09:53:54 +0200

    Profile component workout

--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { User } from '../../models/user';
+import { TruncatePipe } from '../../shared/truncate.pipe';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule, TruncatePipe],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.css'
+  styleUrl: './profile.component.css',
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  userProfile: User | null = null;
+  role: String | null = null;
 
+  constructor(private userService: UserService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.userService.getProfileCard().subscribe({
+      next: (userProfile) => {
+        if (userProfile) {
+          this.userProfile = userProfile;
+
+          this.userService.getLoggedUserRole().subscribe({
+            next: (role) => {
+              this.role = role;
+              console.log(role);
+            },
+            error: (error) => {
+              console.error('Error fetching user role:', error);
+            },
+          });
+        }
+      },
+      error: (error) => {
+        console.error('Error fetching user profile:', error);
+      },
+    });
+  }
+
+  editProfile(form: NgForm) {
+    return;
+  }
 }
